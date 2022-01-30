545e9a5c60676fb55f4e0e3bc18806215e5691e7
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-01-30 19:15:47 +0100

    add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:melodifestivalen_competition/text_form_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MelodifestivalenCompetitionApp());
 }
 
