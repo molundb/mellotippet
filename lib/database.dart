af947080ad991814eed828527c3fb0c53afd2ec7
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-23 00:32:24 +0100

    refactor: add Prediction model<

--- lib/database.dart
+++ lib/database.dart
@@ -1,28 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:melodifestivalen_competition/prediction_model.dart';
 
 class Database {
   FirebaseFirestore db = FirebaseFirestore.instance;
 
   // TODO: Change type String to int for predictions and add input validation
-  void storePrediction(
-    String name,
-    String finalist1,
-    String finalist2,
-    String semifinalist1,
-    String semifinalist2,
-    String fifthPlace,
-  ) async {
+  void uploadPrediction(PredictionModel prediction) async {
     var uid = FirebaseAuth.instance.currentUser?.uid;
 
     if (uid != null) {
       db.collection('predictions').doc(uid).set({
-        "name": name,
-        "finalist1": finalist1,
-        "finalist2": finalist2,
-        "semifinalist3": semifinalist1,
-        "semifinalist4": semifinalist2,
-        "fifthPlace": fifthPlace,
+        "name": prediction.name,
+        "finalist1": prediction.finalist1,
+        "finalist2": prediction.finalist2,
+        "semifinalist3": prediction.semifinalist1,
+        "semifinalist4": prediction.semifinalist2,
+        "fifthPlace": prediction.fifthPlace,
       });
     }
   }
---
+++ lib/prediction_model.dart
@@ -0,0 +1,8 @@
+class PredictionModel {
+  String? name;
+  String? finalist1;
+  String? finalist2;
+  String? semifinalist1;
+  String? semifinalist2;
+  String? fifthPlace;
+}
