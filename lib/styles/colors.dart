c4545db1c406f79e0c42cf457b483599b387b21f
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-24 10:27:35 +0100

    feat: simple design of UpcomingCompetitionRow

---
+++ lib/styles/colors.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+
+const melloLightBlue = Color(0xFFD6098B);
+const melloPurple = Color(0xFF82509F);
+const melloPink = Color(0xFFEB3791);
+const melloMagenta = Color(0xFFD6098B);
+const melloOrange = Color(0xFFF95A28);
+const melloYellow = Color(0xFFFF9F00);
