377a0cd3220aff3862a1a47c520d1d23eb0237ef
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-02-02 23:22:35 +0100

    feat: update appbar title style

--- lib/mello_bottom_navigation_bar.dart
+++ lib/mello_bottom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:melodifestivalen_competition/pages/prediction_page.dart';
+import 'package:melodifestivalen_competition/styles/colors.dart';
 
 class MelloBottomNavigationBar extends StatefulWidget {
   const MelloBottomNavigationBar({super.key});
@@ -28,7 +29,11 @@ class _MelloBottomNavigationBarState extends State<MelloBottomNavigationBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Melodifestivalen Competition'),
+        centerTitle: true,
+        title: const Text(
+          'Mello Predix',
+          style: TextStyle(color: melloYellow, fontSize: 32),
+        ),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
