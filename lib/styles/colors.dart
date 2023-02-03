63b1e57be737095d98847203be2c7e9c4d456fc9
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-02-03 13:59:49 +0100

    feat: add launcher icons

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-const melloLightBlue = Color(0xFFD6098B);
 const melloPurple = Color(0xFF82509F);
 const melloPink = Color(0xFFEB3791);
 const melloMagenta = Color(0xFFD6098B);
