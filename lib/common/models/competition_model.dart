ed01e32bd9512e8e5ed268021cc6085f99170047
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-02-08 22:33:53 +0100

    feat: calculate score for all competitions

--- lib/common/models/competition_model.dart
+++ lib/common/models/competition_model.dart
@@ -3,16 +3,20 @@ import 'package:melodifestivalen_competition/common/models/models.dart';
 import 'package:melodifestivalen_competition/common/models/participant_model.dart';
 
 class CompetitionModel {
+  String id;
   // List<ParticipantModel>? participants;
   // DateTime time;
   // String? locationName;
   Map<String, dynamic> result;
+  // List<PredictionModel> predictions;
 
   CompetitionModel({
+    required this.id,
     // required this.participants,
     // required this.time,
     // required this.locationName,
     required this.result,
+    // required this.predictions,
   });
 
   factory CompetitionModel.fromFirestore(
@@ -21,10 +25,15 @@ class CompetitionModel {
   ) {
     final data = snapshot.data();
     return CompetitionModel(
+      id: snapshot.id,
       // participants: null,
       // time: (data?['time'] as Timestamp).toDate(),
       // locationName: data?['locationName'],
       result: data?['result'],
+      // predictions: data?['predictions']((document) {
+      //   PredictionModel.fromJson(document);
+      // }),
+        //PredictionModel.fromJson(data?['predictions']),
     );
   }
 
