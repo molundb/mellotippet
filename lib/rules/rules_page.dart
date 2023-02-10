36b0637992e0acbfd3982c9a16dce0e88a0773df
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-02-10 14:50:14 +0100

    fix: update rules to be correct

--- lib/rules/rules_page.dart
+++ lib/rules/rules_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:melodifestivalen_competition/styles/colors.dart';
 import 'package:melodifestivalen_competition/styles/text_styles.dart';
 
 class RulesPage extends ConsumerWidget {
@@ -12,12 +13,16 @@ class RulesPage extends ConsumerWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const Text('Rules for Heat 1-4', style: TextStyle(fontSize: 32)),
+            const Text('Rules for Heat 1-4',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: MelloPredixColors.melloYellow,
+                )),
             const SizedBox(height: 16),
             const Text(
-              'Predict which songs will make it to the final, semi-final and fifth place. You can change your prediction as many times as you want - it is the last one that counts. The predictions close shortly before any song is announced to go to the second voting round.'
+              'Predict which songs will make it to the final, semi-final and fifth place. You can change your prediction as many times as you want - it is the last one that counts. The predictions close shortly before the first finalist is announced.'
               ''
-              '\n\nIf you miss predicting in any heat, you get 1p less than the person who got the lowest score in that heat.',
+              '\n\nIf you miss predicting any heat, you get 1p less than the person who got the lowest score in that heat.',
               style: MelloPredixTextStyle.defaultStyle,
             ),
             const SizedBox(height: 32),
@@ -129,27 +134,6 @@ class RulesPage extends ConsumerWidget {
           ],
         ),
       ),
-    );
-  }
-
-  Text buildText() {
-    return const Text(
-      'Bet on which songs will make it to the final, semi-final and fifth place. You can bet as many times as you want - your last bet is what counts. The betting closes shortly before any song is announced to go to the second voting round.'
-      ''
-      '\n\n• For each finalist you had as a finalist: 5p'
-      ''
-      '\n\n• For each finalist you had as a semi-finalist: 3p'
-      ''
-      '\n\n• For each finalist you had as 5th place: 1p'
-      ''
-      '\n\n• For each semi-finalist you had as semi-finalist: 2p'
-      ''
-      '\n\n• For each semi-finalist you had as a finalist: 1p'
-      ''
-      '\n\n• For each semi-finalist you had as 5th place: 1p'
-      ''
-      '\n\nIf you miss betting in any competition, you get 1p less than the person who got the lowest score in that competition.',
-      style: MelloPredixTextStyle.defaultStyle,
     );
   }
 }
