19ff40e5d5cb626d13cf192db0b9a3ef0e1c84c2
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-02-26 22:12:48 +0100

    feat: show correct score in score detail when no predix

--- lib/score/score_for_competition_page.dart
+++ lib/score/score_for_competition_page.dart
@@ -8,7 +8,7 @@ import 'package:melodifestivalen_competition/score/score_controller.dart';
 import 'package:melodifestivalen_competition/styles/colors.dart';
 
 class ScoreForCompetitionPage extends ConsumerStatefulWidget {
-  final PredictionModel result;
+  final CompetitionModel competition;
   final PredictionModel? prediction;
 
   // Result
@@ -16,7 +16,7 @@ class ScoreForCompetitionPage extends ConsumerStatefulWidget {
   // User Score?
 
   const ScoreForCompetitionPage({
-    required this.result,
+    required this.competition,
     required this.prediction,
     super.key,
   });
@@ -33,7 +33,9 @@ class _ScoreForCompetitionPageState
 
   @override
   Widget build(BuildContext context) {
-    final predictionMap = widget.prediction!.toMap();
+    final competition = widget.competition;
+    final prediction = widget.prediction;
+    final predictionMap = prediction?.toMap();
 
     return Scaffold(
       appBar: AppBar(
@@ -48,124 +50,137 @@ class _ScoreForCompetitionPageState
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-        child: CustomScrollView(
-          slivers: [
-            // SliverToBoxAdapter(
-            //   child: Center(
-            //     child: Padding(
-            //       padding: const EdgeInsets.only(bottom: 16.0),
-            //       child: Text(
-            //         widget.userEntity.username ?? '',
-            //         style: const TextStyle(
-            //           fontSize: 32,
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            SliverToBoxAdapter(
-              child: Column(
+        child: prediction == null
+            ? Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text('                    '),
-                      Text('Result'),
-                      Text('Predix'),
-                      Text('Points'),
-                    ],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: MelloPredixColors.melloOrange),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          5.0,
-                        ), //
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Final       '),
-                        Text(widget.result.finalist1.toString()),
-                        Text(predictionMap[widget.result.finalist1] ?? '-'),
-                        Text(
-                            '${calculateFinalistScore(widget.result.finalist1, widget.prediction!)}p'),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: MelloPredixColors.melloOrange),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          5.0,
-                        ), //
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Final       '),
-                        Text(widget.result.finalist2.toString()),
-                        Text(predictionMap[widget.result.finalist2] ?? '-'),
-                        Text(
-                            '${calculateFinalistScore(widget.result.finalist2, widget.prediction!)}p'),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: MelloPredixColors.melloOrange),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          5.0,
-                        ), //
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Semifinal'),
-                        Text(widget.result.semifinalist1.toString()),
-                        Text(predictionMap[widget.result.semifinalist1] ?? '-'),
-                        Text(
-                            '${calculateSemifinalistScore(widget.result.semifinalist1, widget.prediction!)}p'),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: MelloPredixColors.melloOrange),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          5.0,
-                        ), //
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Semifinal'),
-                        Text(widget.result.semifinalist2.toString()),
-                        Text(predictionMap[widget.result.semifinalist2] ?? '-'),
-                        Text(
-                            '${calculateSemifinalistScore(widget.result.semifinalist2, widget.prediction!)}p'),
-                      ],
-                    ),
-                  ),
+                  Text('Your score is ${competition.lowestScore - 1}p',
+                      style: const TextStyle(fontSize: 24)),
                   const SizedBox(height: 16),
+                  const Text(
+                      'You did not make a prediction on time! Therefore, your score is 1 point less than the person who scored the lowest.'),
+                ],
+              )
+            : CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text('                    '),
+                            Text('Result'),
+                            Text('Predix'),
+                            Text('Points'),
+                          ],
+                        ),
+                        _buildRow(
+                          position: "Final       ",
+                          prediction: predictionMap != null
+                              ? predictionMap[competition.result.finalist1]
+                              : null,
+                          score: _getFinalistScore(
+                            competition.lowestScore,
+                            competition.result.finalist1!,
+                            prediction,
+                          ),
+                          result: competition.result.finalist1,
+                        ),
+                        _buildRow(
+                          position: "Final       ",
+                          prediction: predictionMap != null
+                              ? predictionMap[competition.result.finalist1]
+                              : null,
+                          score: _getFinalistScore(
+                            competition.lowestScore,
+                            competition.result.finalist2!,
+                            prediction,
+                          ),
+                          result: competition.result.finalist2,
+                        ),
+                        _buildRow(
+                          position: "Semifinal",
+                          prediction: predictionMap != null
+                              ? predictionMap[competition.result.semifinalist1]
+                              : null,
+                          score: _getSemifinalistScore(
+                            competition.lowestScore,
+                            competition.result.semifinalist1!,
+                            prediction,
+                          ),
+                          result: competition.result.semifinalist1,
+                        ),
+                        _buildRow(
+                          position: "Semifinal",
+                          prediction: predictionMap != null
+                              ? predictionMap[competition.result.semifinalist2]
+                              : null,
+                          score: _getSemifinalistScore(
+                            competition.lowestScore,
+                            competition.result.semifinalist2!,
+                            prediction,
+                          ),
+                          result: competition.result.semifinalist2,
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ),
-          ],
-        ),
       ),
     );
+  }
+
+  Container _buildRow({
+    required String position,
+    required String? prediction,
+    required int score,
+    required int? result,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: MelloPredixColors.melloOrange),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            5.0,
+          ), //
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(position),
+          Text(result.toString()),
+          Text(prediction ?? '-'),
+          Text('${score}p'),
+        ],
+      ),
+    );
+  }
+
+  int _getSemifinalistScore(
+    int lowestScore,
+    int semifinalist,
+    PredictionModel? prediction,
+  ) {
+    if (prediction == null) {
+      return lowestScore - 1;
+    }
+
+    return calculateSemifinalistScore(semifinalist, prediction);
+  }
+
+  int _getFinalistScore(
+    int lowestScore,
+    int finalist,
+    PredictionModel? prediction,
+  ) {
+    if (prediction == null) {
+      return lowestScore - 1;
+    }
+
+    return calculateFinalistScore(finalist, prediction);
   }
 }
