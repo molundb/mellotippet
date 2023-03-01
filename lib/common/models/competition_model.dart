675c92793fb13d14dc73d3d749bdffe0aa1d2ad1
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-03-01 19:44:53 +0100

    refactor: remove commented out code

--- lib/common/models/competition_model.dart
+++ lib/common/models/competition_model.dart
@@ -3,21 +3,13 @@ import 'package:melodifestivalen_competition/common/models/models.dart';
 
 class CompetitionModel {
   String id;
-  // List<ParticipantModel>? participants;
-  // DateTime time;
-  // String? locationName;
   int lowestScore;
   HeatPredictionModel result;
-  // List<PredictionModel> predictions;
 
   CompetitionModel({
     required this.id,
-    // required this.participants,
-    // required this.time,
-    // required this.locationName,
     required this.lowestScore,
     required this.result,
-    // required this.predictions,
   });
 
   factory CompetitionModel.fromFirestore(
@@ -27,23 +19,12 @@ class CompetitionModel {
     final data = snapshot.data();
     return CompetitionModel(
       id: snapshot.id,
-      // participants: null,
-      // time: (data?['time'] as Timestamp).toDate(),
-      // locationName: data?['locationName'],
       lowestScore: data?['lowestScore'] ?? -1,
       result: HeatPredictionModel.fromJson(data?['result']),
-      // predictions: data?['predictions']((document) {
-      //   PredictionModel.fromJson(document);
-      // }),
-        //PredictionModel.fromJson(data?['predictions']),
     );
   }
 
   Map<String, dynamic> toFirestore() {
-    return {
-      // if (participants != null) "participants": participants,
-      // "time": time,
-      // if (locationName != null) "locationName": locationName,
-    };
+    return {};
   }
 }
