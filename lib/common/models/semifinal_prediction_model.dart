052382c98328b383cb4e3b4e3bf43042c61d770d
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-03-01 19:38:52 +0100

    feat: add SemifinalPredictionModel

---
+++ lib/common/models/semifinal_prediction_model.dart
@@ -0,0 +1,46 @@
+class SemifinalPredictionModel {
+  SemifinalPredictionModel({
+    this.finalist1,
+    this.finalist2,
+    this.finalist3,
+    this.finalist4,
+  });
+
+  int? finalist1;
+  int? finalist2;
+  int? finalist3;
+  int? finalist4;
+
+  factory SemifinalPredictionModel.fromJson(Map<String, dynamic> json) =>
+      SemifinalPredictionModel(
+        finalist1: json['finalist1'] ?? -1,
+        finalist2: json['finalist2'] ?? -1,
+        finalist3: json['finalist3'] ?? -1,
+        finalist4: json['finalist4'] ?? -1,
+      );
+
+  SemifinalPredictionModel copyWith({
+    int? finalist1,
+    int? finalist2,
+    int? finalist3,
+    int? finalist4,
+  }) {
+    return SemifinalPredictionModel(
+      finalist1: finalist1 ?? this.finalist1,
+      finalist2: finalist2 ?? this.finalist2,
+      finalist3: finalist3 ?? this.finalist3,
+      finalist4: finalist4 ?? this.finalist4,
+    );
+  }
+}
+
+extension PredictionToMap on SemifinalPredictionModel {
+  Map<int, String> toMap() {
+    return <int, String>{
+      finalist1!: 'F',
+      finalist2!: 'F',
+      finalist3!: 'F',
+      finalist4!: 'F',
+    };
+  }
+}
