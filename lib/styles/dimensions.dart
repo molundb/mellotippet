865079bd1ece1b1b234d2db7d49cba9569a457dc
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-07-31 01:18:33 +0200

    feat: add basic common dimensions in dimensions.dart

---
+++ lib/styles/dimensions.dart
@@ -0,0 +1,20 @@
+class MelloPredixDimensions {
+  static const one = 1.0;
+
+  static const xxxSmall = one;
+  static const xxSmall = 2.0;
+  static const xSmall = 4.0;
+  static const small = 8.0;
+  static const medium = 16.0;
+  static const large = 24.0;
+  static const xLarge = 32.0;
+  static const xxLarge = 48.0;
+  static const xxxLarge = 64.0;
+
+  static const horizontalPadding = medium;
+  static const verticalPadding = large;
+
+  static const cornerRadiusSmall = 6.0;
+  static const cornerRadius = 10.0;
+  static const cornerRadiusLarge = 20.0;
+}
