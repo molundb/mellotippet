b623ae8f38fb5e3dc51a72adbce57fe367e0ae02
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-09-24 23:02:07 +0200

    test: add tests for score controller

--- test/score/score_controller_test.dart
+++ test/score/score_controller_test.dart
@@ -14,50 +14,140 @@ import 'score_controller_test.mocks.dart';
 ])
 void main() {
   late MockDatabaseRepository mockDatabaseRepo;
-  late ScoreController scoreController;
 
   setUp(() {
     mockDatabaseRepo = MockDatabaseRepository();
   });
 
-  test('given two users, when getting user scores, then there are two user scores', () async {
+  test(
+      'given no users, when getting user scores, then there are no user scores',
+      () async {
     // Given
-    scoreController = ScoreController(databaseRepository: mockDatabaseRepo);
+    final scoreController =
+        ScoreController(databaseRepository: mockDatabaseRepo);
+
+    final List<User> fakeUsers = [];
 
     when(mockDatabaseRepo.getUsers())
         .thenAnswer((_) => Future(() => fakeUsers));
     when(mockDatabaseRepo.getCompetitions())
         .thenAnswer((_) => Future(() => fakeCompetitions));
 
-    for (var user in fakeUsers) {
-      for (var competition in fakeCompetitions) {
-        switch (competition.type) {
-          case CompetitionType.heat:
-            when(mockDatabaseRepo.getPredictionsForHeatForUser(
-              competition.id,
-              user.id,
-            )).thenAnswer((_) => Future(() => fakeHeatPrediction));
-            break;
-          case CompetitionType.semifinal:
-            when(mockDatabaseRepo.getPredictionsForSemifinalForUser(
-              competition.id,
-              user.id,
-            )).thenAnswer((_) => Future(() => fakeSemifinalPrediction));
-            break;
-          case CompetitionType.theFinal:
-            when(mockDatabaseRepo.getPredictionsForFinalForUser(
-              competition.id,
-              user.id,
-            )).thenAnswer((_) => Future(() => fakeFinalPrediction));
-            break;
-        }
-      }
-    }
+    mockGetPredictionsCalls(fakeUsers, fakeCompetitions, mockDatabaseRepo);
 
     // When
     await scoreController.getUserScores();
 
     // Then
-    expect(scoreController.state.userScores.length, fakeUsers.length);
+    expect(scoreController.state.userScores.length, 0);
   });
+
+  test('given no competition, when getting user scores, then all scores are 0',
+      () async {
+    // Given
+    final scoreController =
+        ScoreController(databaseRepository: mockDatabaseRepo);
+
+    final fakeUsers = [
+      const User(id: "id1", username: "username1"),
+      const User(id: "id2", username: "username2"),
+    ];
+
+    final List<CompetitionModel> fakeCompetitions = [];
+
+    when(mockDatabaseRepo.getUsers())
+        .thenAnswer((_) => Future(() => fakeUsers));
+    when(mockDatabaseRepo.getCompetitions())
+        .thenAnswer((_) => Future(() => fakeCompetitions));
+
+    mockGetPredictionsCalls(fakeUsers, fakeCompetitions, mockDatabaseRepo);
+
+    // When
+    await scoreController.getUserScores();
+
+    // Then
+    for (final score in scoreController.state.userScores) {
+      expect(score.totalScore, 0);
+    }
+  });
+
+  test(
+      'given two users, when getting user scores, then there are two user scores',
+      () async {
+    // Given
+    final scoreController =
+        ScoreController(databaseRepository: mockDatabaseRepo);
+
+    final fakeUsers = [
+      const User(id: "id1", username: "username1"),
+      const User(id: "id2", username: "username2"),
+    ];
+
+    when(mockDatabaseRepo.getUsers())
+        .thenAnswer((_) => Future(() => fakeUsers));
+    when(mockDatabaseRepo.getCompetitions())
+        .thenAnswer((_) => Future(() => fakeCompetitions));
+
+    mockGetPredictionsCalls(fakeUsers, fakeCompetitions, mockDatabaseRepo);
+
+    // When
+    await scoreController.getUserScores();
+
+    // Then
+    expect(scoreController.state.userScores.length, 2);
+  });
+
+  test(
+      'given user with null username, when getting user scores, then there is no score for that user',
+      () async {
+    // Given
+    final scoreController =
+        ScoreController(databaseRepository: mockDatabaseRepo);
+
+    final fakeUsers = [
+      const User(id: "id1", username: "username1"),
+      const User(id: "id2", username: "username2"),
+      const User(id: "id3", username: null),
+    ];
+
+    when(mockDatabaseRepo.getUsers())
+        .thenAnswer((_) => Future(() => fakeUsers));
+    when(mockDatabaseRepo.getCompetitions())
+        .thenAnswer((_) => Future(() => fakeCompetitions));
+
+    mockGetPredictionsCalls(fakeUsers, fakeCompetitions, mockDatabaseRepo);
+
+    // When
+    await scoreController.getUserScores();
+
+    // Then
+    expect(scoreController.state.userScores.length, 2);
+  });
+}
+
+void mockGetPredictionsCalls(List<User> fakeUsers, List<CompetitionModel> fakeCompetitions, MockDatabaseRepository mockDatabaseRepo) {
+  for (final user in fakeUsers) {
+    for (final competition in fakeCompetitions) {
+      switch (competition.type) {
+        case CompetitionType.heat:
+          when(mockDatabaseRepo.getPredictionsForHeatForUser(
+            competition.id,
+            user.id,
+          )).thenAnswer((_) => Future(() => fakeHeatPrediction));
+          break;
+        case CompetitionType.semifinal:
+          when(mockDatabaseRepo.getPredictionsForSemifinalForUser(
+            competition.id,
+            user.id,
+          )).thenAnswer((_) => Future(() => fakeSemifinalPrediction));
+          break;
+        case CompetitionType.theFinal:
+          when(mockDatabaseRepo.getPredictionsForFinalForUser(
+            competition.id,
+            user.id,
+          )).thenAnswer((_) => Future(() => fakeFinalPrediction));
+          break;
+      }
+    }
+  }
 }
