f7d3b604e5f5555ad40e55c0363ade8b471288e0
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-10-07 18:09:02 +0200

    feat: add firebase functions methods for updating and deleting dummy entries

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -4,16 +4,17 @@ import {db} from "./config/firebase";
 type EntryType = {
     title: string,
     text: string,
-    coverImageUrl: string
+    coverImageUrl: string,
   }
 
-  type Request = {
+type Request = {
     body: EntryType,
     params: { entryId: string }
-  }
+}
 
 const addEntry = async (req: Request, res: Response) => {
   const {title, text} = req.body;
+
   try {
     const entry = db.collection("entries").doc();
     const entryObject = {
@@ -30,8 +31,72 @@ const addEntry = async (req: Request, res: Response) => {
       data: entryObject,
     });
   } catch (error) {
-    res.status(500).json("There was an error posting your request.");
+    res.status(500).json("There was an error posting your entry.");
   }
 };
 
-export {addEntry};
+const getAllEntries = async (req: Request, res: Response) => {
+  try {
+    const allEntries: EntryType[] = [];
+
+    (await db.collection("entries").get())
+        .forEach((doc: any) => allEntries.push(doc.data()));
+    return res.status(200).json(allEntries);
+  } catch (error) {
+    return res.status(500).json("There was an error fetching all entries.");
+  }
+};
+
+const updateEntry = async (req: Request, res: Response) => {
+  const {body: {text, title}, params: {entryId}} = req;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+    const currentData = (await entry.get()).data() || {};
+    const entryObject = {
+      title: title || currentData.title,
+      text: text || currentData.text,
+    };
+
+    await entry.set(entryObject).catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry updated successfully",
+      data: entryObject,
+    });
+  } catch (error) {
+    return res.status(500).json(
+        `There was an error updating the entry: ${error}`
+    );
+  }
+};
+
+const deleteEntry = async (req: Request, res: Response) => {
+  const {entryId} = req.params;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+
+    await entry.delete().catch((error) => {
+      return res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "entry deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json("We found an error deleting an entry");
+  }
+};
+
+export {addEntry, getAllEntries, updateEntry, deleteEntry};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,18 @@
 import {onRequest} from "firebase-functions/v2/https";
 import express from "express";
-import {addEntry} from "./entryController";
+import {
+  addEntry,
+  getAllEntries,
+  updateEntry,
+  deleteEntry,
+} from "./entryController";
 
 const app2 = express();
 
 app2.get("/", (req, res) => res.status(200).send("Hey there!"));
 app2.post("/entries", addEntry);
+app2.get("/entries", getAllEntries);
+app2.patch("/entries/:entryId", updateEntry);
+app2.delete("/entries/:entryId", deleteEntry);
 
 exports.app2 = onRequest(app2);
