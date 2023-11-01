2e0f968bde320dfbf0c46cee8852ba48aa5d1958
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-11-01 22:33:45 +0100

    test: add first firestore rules test

---
+++ functions/test/firestore-rules.spec.ts
@@ -0,0 +1,14 @@
+import * as firebase from "@firebase/testing";
+
+const MY_PROJECT_ID = "melodifestivalen-comp-test";
+
+describe("Tests for rules", () => {
+  it.only("Can't write", async () => {
+    const db = firebase
+      .initializeTestApp({ projectId: MY_PROJECT_ID })
+      .firestore();
+
+    const testDoc = db.collection("users").doc("user1");
+    await firebase.assertFails(testDoc.set({ foo: "bar" }));
+  });
+});
