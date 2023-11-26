4c4eb74f6be392bd29423a98b32de90b7bf2cb41
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-11-26 09:56:59 +0100

    refactor: create model/prediction folder

--- lib/common/models/all_models.dart
+++ lib/common/models/all_models.dart
@@ -1,9 +1,9 @@
 export 'auth_error.dart';
 export 'competition_model.dart';
-export 'heat_prediction_model.dart';
+export 'prediction/heat_prediction_model.dart';
 export 'participant_model.dart';
 export 'prediction_model.dart';
-export 'semifinal_prediction_model.dart';
+export 'prediction/semifinal_prediction_model.dart';
 export 'user_entity_two.dart';
 export 'user_score_entity.dart';
-export 'final_prediction_model.dart';
+export 'prediction/final_prediction_model.dart';
