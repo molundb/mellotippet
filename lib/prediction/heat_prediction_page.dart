2d0c1737e5660896bf4c101ae6a82f3d99a22d52
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-12-03 00:04:15 +0100

    feat: fix tippa button at bottom of page

--- lib/prediction/heat_prediction_page.dart
+++ lib/prediction/heat_prediction_page.dart
@@ -38,80 +38,88 @@ class _HeatPredictionPageState extends ConsumerState<HeatPredictionPage> {
 
     // TODO: Figure out how to handle loading
 
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints viewportConstraints) {
-        return SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-            child: Form(
-              key: _formKey,
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: viewportConstraints.maxHeight,
+    return Column(
+      children: [
+        Expanded(
+          child: LayoutBuilder(
+            builder:
+                (BuildContext context, BoxConstraints viewportConstraints) {
+              return SingleChildScrollView(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+                  child: Form(
+                    key: _formKey,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minHeight: viewportConstraints.maxHeight,
+                      ),
+                      child: Column(
+                        children: [
+                          const Center(child: Text('Final')),
+                          const SizedBox(height: 8.0),
+                          DragTargetRow(
+                            row: state.predictions[0],
+                            index: 0,
+                            emptyText: "Finalist",
+                            setRow: controller.setRow,
+                            clearRow: controller.clearRow,
+                          ),
+                          const SizedBox(height: 8.0),
+                          DragTargetRow(
+                            row: state.predictions[1],
+                            index: 1,
+                            emptyText: "Finalist",
+                            setRow: controller.setRow,
+                            clearRow: controller.clearRow,
+                          ),
+                          const SizedBox(height: 8.0),
+                          const Center(child: Text('Semifinal')),
+                          const SizedBox(height: 8.0),
+                          DragTargetRow(
+                            row: state.predictions[2],
+                            index: 2,
+                            emptyText: "Semifinalist",
+                            setRow: controller.setRow,
+                            clearRow: controller.clearRow,
+                          ),
+                          const SizedBox(height: 8.0),
+                          DragTargetRow(
+                            row: state.predictions[3],
+                            index: 3,
+                            emptyText: "Semifinalist",
+                            setRow: controller.setRow,
+                            clearRow: controller.clearRow,
+                          ),
+                          const SizedBox(height: 8.0),
+                          const Center(child: Text('Plats 5')),
+                          const SizedBox(height: 8.0),
+                          DragTargetRow(
+                            row: state.predictions[4],
+                            index: 4,
+                            emptyText: "Plats 5",
+                            setRow: controller.setRow,
+                            clearRow: controller.clearRow,
+                          ),
+                          const SizedBox(height: 8.0),
+                          const Center(child: Text('Övriga')),
+                          const SizedBox(height: 8.0),
+                          OtherList(others: state.others),
+                          const SizedBox(height: 8.0),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                child: Column(
-                  children: [
-                    const Center(child: Text('Final')),
-                    const SizedBox(height: 8.0),
-                    DragTargetRow(
-                      row: state.predictions[0],
-                      index: 0,
-                      emptyText: "Finalist",
-                      setRow: controller.setRow,
-                      clearRow: controller.clearRow,
-                    ),
-                    const SizedBox(height: 8.0),
-                    DragTargetRow(
-                      row: state.predictions[1],
-                      index: 1,
-                      emptyText: "Finalist",
-                      setRow: controller.setRow,
-                      clearRow: controller.clearRow,
-                    ),
-                    const SizedBox(height: 8.0),
-                    const Center(child: Text('Semifinal')),
-                    const SizedBox(height: 8.0),
-                    DragTargetRow(
-                      row: state.predictions[2],
-                      index: 2,
-                      emptyText: "Semifinalist",
-                      setRow: controller.setRow,
-                      clearRow: controller.clearRow,
-                    ),
-                    const SizedBox(height: 8.0),
-                    DragTargetRow(
-                      row: state.predictions[3],
-                      index: 3,
-                      emptyText: "Semifinalist",
-                      setRow: controller.setRow,
-                      clearRow: controller.clearRow,
-                    ),
-                    const SizedBox(height: 8.0),
-                    const Center(child: Text('Plats 5')),
-                    const SizedBox(height: 8.0),
-                    DragTargetRow(
-                      row: state.predictions[4],
-                      index: 4,
-                      emptyText: "Plats 5",
-                      setRow: controller.setRow,
-                      clearRow: controller.clearRow,
-                    ),
-                    const SizedBox(height: 8.0),
-                    const Center(child: Text('Övriga')),
-                    const SizedBox(height: 8.0),
-                    OtherList(others: state.others),
-                    const SizedBox(height: 8.0),
-                    CtaButton(
-                      text: "Tippa",
-                      onPressed: controller.submitPrediction,
-                    )
-                  ],
-                ),
-              ),
-            ),
+              );
+            },
           ),
-        );
-      },
+        ),
+        CtaButton(
+          text: "Tippa",
+          onPressed: controller.submitPrediction,
+        )
+      ],
     );
   }
 
