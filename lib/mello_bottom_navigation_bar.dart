ac9b1e8b198f00b55b89565e23d1ceba6dc0e3b7
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-12-10 10:41:46 +0100

    feat: update navigation bar to match the design

--- lib/mello_bottom_navigation_bar.dart
+++ lib/mello_bottom_navigation_bar.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mellotippet/config/config.dart';
 import 'package:mellotippet/prediction/heat_prediction_page.dart';
-import 'package:mellotippet/rules/final_rules_page.dart';
 import 'package:mellotippet/service_location/get_it.dart';
 import 'package:mellotippet/snackbar/snackbar_handler.dart';
 import 'package:mellotippet/styles/colors.dart';
 
 import 'score/score_page.dart';
-import 'settings/settings_page.dart';
 
 class MelloBottomNavigationBar extends StatefulWidget {
   const MelloBottomNavigationBar({super.key});
@@ -22,11 +20,8 @@ class _MelloBottomNavigationBarState extends State<MelloBottomNavigationBar> {
 
   int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
-    HeatPredictionPage(snackbarHandler: getIt.get<SnackbarHandler>()),
-    // UpcomingCompetitionsPage(),
-    const FinalRulesPage(),
     const ScorePage(),
-    const SettingsPage(),
+    HeatPredictionPage(snackbarHandler: getIt.get<SnackbarHandler>()),
   ];
 
   @override
@@ -47,30 +42,20 @@ class _MelloBottomNavigationBarState extends State<MelloBottomNavigationBar> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        backgroundColor: MellotippetColors.melloPurple,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: MellotippetColors.melloLightOrange,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit_note_sharp),
-            label: 'Tippa',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.calendar_month),
-          //   label: 'Upcoming',
-          // ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt),
-            label: 'Rules',
-          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.star),
             label: 'Score',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_box_rounded),
-            label: 'Settings',
+            icon: Icon(Icons.edit_note_sharp),
+            label: 'Tippa',
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
       ),
     );
