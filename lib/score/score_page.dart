6b94bac5e2730e028530770d8b8405fac8a8584e
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-12-10 09:44:04 +0100

    feat: add score page background image

--- lib/score/score_page.dart
+++ lib/score/score_page.dart
@@ -30,76 +30,16 @@ class _ScorePageState extends ConsumerState<ScorePage> {
 
     // TODO: Figure out how to handle loading
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-      child: CustomScrollView(
-        slivers: [
-          const SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Text(
-                  'Highscore',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: MellotippetColors.melloLightOrange,
-                  ),
-                ),
-                SizedBox(height: 16),
-              ],
-            ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/score_page_background.png',
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: state.userScores.length,
-              (context, index) => Column(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute<bool>(
-                          builder: (BuildContext context) =>
-                              ScorePerCompetitionPage(
-                                  userEntity: state.userScores[index]),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: MellotippetColors.melloDarkOrange),
-                        borderRadius: const BorderRadius.all(Radius.circular(
-                                5.0) //                 <--- border radius here
-                            ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '${index + 1}. ${state.userScores[index].username ?? ''}',
-                              style: const TextStyle(fontSize: 24),
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          const SizedBox(width: 16),
-                          Text(
-                            '${state.userScores[index].totalScore}p',
-                            style: const TextStyle(fontSize: 24),
-                          ),
-                          const Icon(Icons.arrow_forward_ios_rounded),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                ],
-              ),
-            ),
-          ),
-        ],
+          fit: BoxFit.cover,
+        ),
       ),
+      child: Container(),
     );
   }
 }
