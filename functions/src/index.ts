30e4552332e598e90b0a54959526575a4cc8e222
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-02-06 21:23:24 +0100

    refactor: remove unused express app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,3 @@
-import { onRequest } from "firebase-functions/v2/https";
-import express from "express";
-import {
-  addEntry,
-  getAllEntries,
-  updateEntry,
-  deleteEntry,
-} from "./entry-controller";
-
 import { calculateScores } from "./score-controller";
 
-const expressApp = express();
-
-expressApp.get("/", (req, res) => res.status(200).send("Hey there!"));
-expressApp.post("/entries", addEntry);
-expressApp.get("/entries", getAllEntries);
-expressApp.patch("/entries/:entryId", updateEntry);
-expressApp.delete("/entries/:entryId", deleteEntry);
-
-const app = onRequest(expressApp);
-
-export { app, calculateScores };
+export { calculateScores };
