c2809fb0c7c996395df457e39d36ac283f7438c1
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-02-07 09:45:03 +0100

    fix: remove textTheme from theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mellotippet/styles/colors.dart';
-import 'package:mellotippet/styles/text_styles.dart';
 
 @immutable
 class MelloTippetTheme {
@@ -19,11 +18,6 @@ class MelloTippetTheme {
         ),
       ),
       fontFamily: 'Roboto',
-      textTheme: const TextTheme(
-        bodyLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        bodyMedium: MellotippetTextStyle.defaultStyle,
-        bodySmall: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
-      ),
     );
   }
 }
